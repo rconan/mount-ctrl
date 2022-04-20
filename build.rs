95608169526060daa1784cf45af22fc1623edd70
Rod Conan <rconan@gmto.org>
2022-04-20 11:51:07 -0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -7,9 +7,5 @@ fn main() {
         .compile("mount_pdr_controller");
     cc::Build::new()
         .file("src/drives/Mount_Drv_PDR2021.c")
-        .file("src/drives/Mount_Drv_PDR2021_data.c")
-        .file("src/drives/rtGetInf.c")
-        .file("src/drives/rtGetNaN.c")
-        .file("src/drives/rt_nonfinite.c")
         .compile("mount_pdr_drives");
 }
