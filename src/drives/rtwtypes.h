17c4bba9219e996a5cee67e80517e66ad42e04cf
Rodrigo Alvite Romano <rovite@gmail.com>
2022-04-20 15:37:02 -0300

    Linear mount drive model

--- src/drives/rtwtypes.h
+++ src/drives/rtwtypes.h
@@ -3,9 +3,9 @@
  *
  * Code generated for Simulink model 'Mount_Drv_PDR2021'.
  *
- * Model version                  : 1.980
+ * Model version                  : 1.1193
  * Simulink Coder version         : 9.0 (R2018b) 24-May-2018
- * C/C++ source code generated on : Wed Mar  9 16:24:57 2022
+ * C/C++ source code generated on : Wed Apr 20 15:30:05 2022
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: Intel->x86-64 (Linux 64)
