f9f399e48b5de1240d56bdbd1e06e6125a4fc4e3
Rod Conan <rconan@gmto.org>
2022-06-28 21:58:31 -0700

    CONTROLLER ENUM
    A single controller enum with variants for controller at 0, 30 and 60
    zenith

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,81 @@
 pub mod controller;
 pub mod drives;
+
+pub mod ze00;
+pub mod ze30;
+pub mod ze60;
+
+pub enum Controller<'a> {
+    Ze00(ze00::Controller<'a>),
+    Ze30(ze30::Controller<'a>),
+    Ze60(ze60::Controller<'a>),
+}
+
+impl<'a> Controller<'a> {
+    pub fn mount_fb(&mut self) -> Option<&mut [f64; 14]> {
+        match self {
+            Controller::Ze00(controller) => {
+                if let ze00::U::MountFB(val) = &mut controller.mount_fb {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+            Controller::Ze30(controller) => {
+                if let ze30::U::MountFB(val) = &mut controller.mount_fb {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+            Controller::Ze60(controller) => {
+                if let ze60::U::MountFB(val) = &mut controller.mount_fb {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+    pub fn mount_sp(&mut self) -> Option<&mut [f64; 3]> {
+        match self {
+            Controller::Ze00(controller) => {
+                if let ze00::U::MountSP(val) = &mut controller.mount_sp {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+            Controller::Ze30(controller) => {
+                if let ze30::U::MountSP(val) = &mut controller.mount_sp {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+            Controller::Ze60(controller) => {
+                if let ze60::U::MountSP(val) = &mut controller.mount_sp {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+    pub fn mount_cmd(&mut self) -> Option<&[f64; 3]> {
+        match self {
+            Controller::Ze00(controller) => {
+                let ze00::Y::Mountcmd(val) = &controller.mount_cmd;
+                Some(val)
+            }
+            Controller::Ze30(controller) => {
+                let ze30::Y::Mountcmd(val) = &controller.mount_cmd;
+                Some(val)
+            }
+            Controller::Ze60(controller) => {
+                let ze60::Y::Mountcmd(val) = &controller.mount_cmd;
+                Some(val)
+            }
+        }
+    }
+}
