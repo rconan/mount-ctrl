b769974c07db3188e1a4365f364c86a028b670e7
Rod Conan <rconan@gmto.org>
2022-06-28 22:08:34 -0700

    ADD ORIGINAL CONTROLLER AS DEFAULT VARIANT TO CONTROLLER ENUM

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 pub mod controller;
 pub mod drives;
-
 pub mod ze00;
 pub mod ze30;
 pub mod ze60;
@@ -9,6 +8,7 @@ pub enum Controller<'a> {
     Ze00(ze00::Controller<'a>),
     Ze30(ze30::Controller<'a>),
     Ze60(ze60::Controller<'a>),
+    Default(controller::Controller<'a>),
 }
 
 impl<'a> Controller<'a> {
@@ -30,6 +30,13 @@ impl<'a> Controller<'a> {
             }
             Controller::Ze60(controller) => {
                 if let ze60::U::MountFB(val) = &mut controller.mount_fb {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
+            Controller::Default(controller) => {
+                if let controller::U::MountFB(val) = &mut controller.mount_fb {
                     Some(val)
                 } else {
                     None
@@ -60,6 +67,13 @@ impl<'a> Controller<'a> {
                     None
                 }
             }
+            Controller::Default(controller) => {
+                if let controller::U::MountSP(val) = &mut controller.mount_sp {
+                    Some(val)
+                } else {
+                    None
+                }
+            }
         }
     }
     pub fn mount_cmd(&mut self) -> Option<&[f64; 3]> {
@@ -74,6 +88,10 @@ impl<'a> Controller<'a> {
             }
             Controller::Ze60(controller) => {
                 let ze60::Y::Mountcmd(val) = &controller.mount_cmd;
+                Some(val)
+            }
+            Controller::Default(controller) => {
+                let controller::Y::Mountcmd(val) = &controller.mount_cmd;
                 Some(val)
             }
         }
