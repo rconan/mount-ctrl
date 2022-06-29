2ca1251aeb33f6346e7426d25271f516424aa04c
Rod Conan <rconan@gmto.org>
2022-06-29 07:49:49 -0700

    ADD METHODS TO CONTROLLER ENUM
    Add new, at_zenith and at_elevation constructor methods

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -10,7 +10,36 @@ pub enum Controller<'a> {
     Default(controller::Controller<'a>),
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum ControlError {
+    #[error("Expected zenith angle 0, 30 or 60 found {0}")]
+    ZenithAngle(i32),
+}
+
+type Result<T> = std::result::Result<T, ControlError>;
+
 impl<'a> Controller<'a> {
+    /// Returns a default mount controller
+    pub fn new() -> Self {
+        Controller::Default(controller::Controller::new())
+    }
+    /// Returns a mount controller for the given zenith angle
+    ///
+    /// The zenith angle is either 0, 30 or 60 degrees
+    pub fn at_zenith_angle(ze: i32) -> Result<Self> {
+        match ze {
+            ze if ze == 0 => Ok(Self::Ze00(ze00::Controller::new())),
+            ze if ze == 30 => Ok(Self::Ze30(ze30::Controller::new())),
+            ze if ze == 60 => Ok(Self::Ze60(ze60::Controller::new())),
+            _ => Err(ControlError::ZenithAngle(ze)),
+        }
+    }
+    /// Returns a mount controller for the given elevation
+    ///
+    /// The elevation is either 90, 60 or 30 degrees
+    pub fn at_elevation(ze: i32) -> Result<Self> {
+        Self::at_zenith_angle(90 - ze)
+    }
     pub fn mount_fb(&mut self) -> Option<&mut [f64; 14]> {
         match self {
             Controller::Ze00(controller) => {
